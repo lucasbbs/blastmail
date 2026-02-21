0a1b443e3aee3007e736f8cfc8eefded77be9d6e
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2026-02-21 11:09:46 -0500

    feat(components): add blade ui components

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,14 +1,21 @@
 @props([
 'post' => null,
-
+'delete' => null,
+'flat' => false
 ])
 
 @php
-$method = $post ? 'POST' : 'GET';
+$method = ($post or $delete) ? 'POST' : 'GET';
 @endphp
 
-<form {{ $attributes->merge(['class' => 'flex flex-col gap-4']) }} method="{{ $method }}">
+<form {{ $attributes->class(['flex flex-col gap-4' => ! $flat]) }} method="{{ $method }}">
+  @if ($method != 'GET')
   @csrf
+  @endif
+
+  @if ($delete)
+  @method('DELETE')
+  @endif
 
   {{ $slot }}
 </form>

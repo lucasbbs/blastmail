34489a8d52a8e9622383a54f440d0e6f861e309d
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2026-03-14 15:51:04 -0400

    Creating the Alerts Component

--- resources/views/campaigns/create/_schedule.blade.php
+++ resources/views/campaigns/create/_schedule.blade.php
@@ -1,8 +1,27 @@
-<div class="grid grid-cols-2 gap-4">
+<div class="flex flex-col gap-4">
+  <x-alert success :title="__('Your campaign is ready to be send!')" />
+
   <div>
+    <div>De: ---@---.com</div>
+    <div>Para: #count de emails do email lists id</div>
+    <div>Assunt: {{ $data['subject'] }}</div>
+    <div>Template: #template</div>
+  </div>
+
+  <hr />
+
+  <div>
+    <x-input-label :value="__('Schedule Deleviry')" />
+    <div class="flex flex-col gap-2 mt-2">
+      <x-input.radio id="send_now" name="send_when" value="now">{{ __('Send Now') }}</x-input.radio>
+      <x-input.radio id="send_later" name="send_when" value="later">{{ __('Send Later') }}</x-input.radio>
+    </div>
+  </div>
+
+  {{-- <div>
     <x-input-label for="send_at" :value="__('Send at')" />
     <x-input.text id="send_at" class="block mt-1 w-full" type="date" name="send_at"
       :value="old('send_at', $data['send_at'])" autofocus />
     <x-input-error :messages="$errors->get('send_at')" class="mt-2" />
-  </div>
+  </div> --}}
 </div>
---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,46 @@
+@props([
+'title' => null,
+'success' => null,
+'info' => null,
+'warning' => null,
+'danger' => null,
+])
+
+<div
+  @class([ 'relative w-full overflow-hidden rounded-xl border bg-white text-slate-700 dark:bg-slate-900 dark:text-slate-300'
+  , 'border-green-600'=> $success,
+  'border-sky-600' => $info,
+  'border-amber-600' => $warning,
+  'border-red-600' => $danger,
+  ])
+  role="alert">
+  <div @class([ 'flex w-full items-center gap-2 p-4' , 'bg-green-500/10'=> $success,
+    'bg-sky-500/10' => $info,
+    'bg-amber-500/10' => $warning,
+    'bg-red-500/10' => $danger,
+    ])>
+    <div @class([ 'rounded-full p-1' , 'text-green-500 bg-green-500/15'=> $success,
+      'text-sky-500 bg-sky-500/15' => $info,
+      'text-amber-500 bg-sky-500/15' => $warning,
+      'text-red-500 bg-sky-500/15' => $danger,
+      ]) aria-hidden="true">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-6" aria-hidden="true">
+        <path fill-rule="evenodd"
+          d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z"
+          clip-rule="evenodd" />
+      </svg>
+    </div>
+    <div class="ml-2">
+      <h3 @class([ 'text-sm font-semibold' , 'text-green-500'=> $success,
+        'text-sky-500' => $info,
+        'text-amber-500' => $warning,
+        'text-red-500' => $danger,
+        ])>{{ $title }}</h3>
+      <p class="text-xs font-medium sm:text-sm">
+        @if ($slot)
+        {{ $slot }}
+        @endif
+      </p>
+    </div>
+  </div>
+</div>

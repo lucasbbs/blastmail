b65cb812055306ac88435bfabead0d7d567e68df
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2026-03-14 16:26:11 -0400

    Adding Shipping Information

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -32,12 +32,28 @@ class CampaignController extends Controller
 
   public function create(?string $tab = null)
   {
+    $data = session()->get('campaigns::create', [
+      'name' =>  null,
+      'subject' =>  null,
+      'email_list_id' => null,
+      'template_id' => null,
+      'body' => null,
+      'track_click' => null,
+      'track_open' => null,
+      'send_at' => null,
+      'send_when' => 'now',
+    ]);
+
     return view(
       'campaigns.create',
       array_merge(
         $this->when(blank($tab), fn() => [
           'emailLists' => EmailList::query()->select(['id', 'title'])->orderBy('title')->get(),
           'templates' => Template::query()->select(['id', 'name'])->orderBy('name')->get(),
+        ], fn() => []),
+        $this->when($tab == 'schedule', fn() => [
+          'countEmails' => EmailList::find($data['email_list_id'])->subscribers()->count(),
+          'template' => Template::find($data['template_id'])->name,
         ], fn() => []),
         [
           'tab' => $tab,
@@ -46,17 +62,7 @@ class CampaignController extends Controller
             'schedule' => '_schedule',
             default => '_config'
           },
-          'data' => session()->get('campaigns::create', [
-            'name' =>  null,
-            'subject' =>  null,
-            'email_list_id' => null,
-            'template_id' => null,
-            'body' => null,
-            'track_click' => null,
-            'track_open' => null,
-            'send_at' => null,
-            'send_when' => 'now',
-          ])
+          'data' => $data
         ]
       )
     );
--- resources/views/campaigns/create/_schedule.blade.php
+++ resources/views/campaigns/create/_schedule.blade.php
@@ -1,14 +1,16 @@
 <div class="flex flex-col gap-4">
   <x-alert success :title="__('Your campaign is ready to be send!')" />
 
-  <div>
-    <div>De: ---@---.com</div>
-    <div>Para: #count de emails do email lists id</div>
-    <div>Assunt: {{ $data['subject'] }}</div>
-    <div>Template: #template</div>
+  <div class="space-y-2">
+    <div>{{ __('From') }}: {{ config('mail.from.address') }}</div>
+    <div>{{ __('To') }}: <x-badge>{{ $countEmails }} Emails</x-badge>
+    </div>
+    <div>{{ __('Subject') }}: {{ $data['subject'] }}</div>
+    <div>{{ __('Template') }}: <x-badge>{{ $template }}</x-badge>
+    </div>
   </div>
 
-  <hr />
+  <hr class="my-3 opacity-20" />
 
   <div x-data="{ show: '{{ data_get($data, 'send_when', 'now') }}' }">
     <x-input-label :value="__('Schedule Deleviry')" />
@@ -17,8 +19,6 @@
       <x-input.radio id="send_now" name="send_when" value="now" x-model="show">{{ __('Send Now') }}</x-input.radio>
       <x-input.radio id="send_later" name="send_when" value="later" x-model="show">{{ __('Send Later') }}
       </x-input.radio>
-
-
 
       <div x-show="show == 'later'">
         <x-input.text id="send_at" class="block mt-1 w-full" type="date" name="send_at"

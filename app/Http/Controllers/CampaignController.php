a5fb8cbdc0ca34ee7f1561c4dd84dccf548fe598
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2026-03-14 09:52:22 -0400

    Showing the respective form in each tab

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -25,36 +25,91 @@ class CampaignController extends Controller
     ]);
   }
 
-
   public function create(?string $tab = null)
   {
+    // session()->forget('campaigns::create');
+
     return view('campaigns.create', [
       'tab' => $tab,
       'form' => match ($tab) {
         'template' => '_template',
         'schedule' => '_schedule',
         default => '_config'
-      }
+      },
+      'data' => session()->get('campaigns::create', [
+        'name' =>  null,
+        'subject' =>  null,
+        'email_list_id' => null,
+        'template_id' => null,
+        'body' => null,
+        'track_click' => null,
+        'track_open' => null,
+        'sent_at' => null
+      ])
     ]);
   }
 
   public function store(?string $tab = null)
   {
+    $toRoute = '';
 
+    $map = array_merge([
+      'name' =>  null,
+      'subject' =>  null,
+      'email_list_id' => null,
+      'template_id' => null,
+      'body' => null,
+      'track_click' => null,
+      'track_open' => null,
+      'sent_at' => null
+    ], request()->all());
 
     if (blank($tab)) {
-      $data = request()->validate([
+      request()->validate([
         'name' => ['required', 'max:255'],
         'subject' => ['required', 'max:40'],
         'email_list_id' => ['nullable'],
         'template_id' => ['nullable'],
+        'body' => ['nullable'],
+        'track_click' => ['nullable'],
+        'track_open' => ['nullable'],
+        'sent_at' => ['nullable']
       ]);
 
-      session()->put('campaigns::create', $data);
-
-      return to_route('campaigns.create', ['tab' => 'template']);
+      $toRoute = route('campaigns.create', ['tab' => 'template']);
     }
+
+    if ($tab == 'template') {
+      request()->validate([
+        'body' => ['required'],
+      ]);
+
+      $toRoute = route('campaigns.create', ['tab' => 'schedule']);
+    }
+
+    if ($tab == 'schedule') {
+      request()->validate([
+        'sent_at' => ['required', 'date'],
+      ]);
+
+      $toRoute = route('campaigns.index');
+    }
+
+    $session = session('campaigns::create');
+
+    foreach ($session as $key => $value) {
+      $newValue = data_get($map, $key);
+
+      if (filled($newValue)) {
+        $session[$key] = $newValue;
+      }
+    }
+
+    session()->put('campaigns::create', $session);
+
+    return response()->redirectTo($toRoute);
   }
+
   public function destroy(Campaign $campaign)
   {
     $campaign->delete();
